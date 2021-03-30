d290970eb4b9a04d2df0b5122f3d8c1c7b59556c
naisy <takanashi@gclue.jp>
2021-03-30 16:45:06 +0900

    add reverse pulse settings in config.yml

--- lib/rccar.py
+++ lib/rccar.py
@@ -12,12 +12,14 @@ class RCCar():
 
     def __init__(self, cfg):
         self.STEERING_NEUTRAL     = cfg['servo_neutral_angle']
-        self.MIN_STEERING_ANGLE   = cfg['servo_min_angle_limit'] # Steering left/right max angle.
-        self.MAX_STEERING_ANGLE   = cfg['servo_max_angle_limit'] # Steering left/right max angle.
+        self.STEERING_REVERSE = cfg['servo_reverse']
+        self.MIN_STEERING_ANGLE_LIMIT   = cfg['servo_min_angle_limit'] # Steering left/right max angle.
+        self.MAX_STEERING_ANGLE_LIMIT   = cfg['servo_max_angle_limit'] # Steering left/right max angle.
         self.STEERING_RATE        = cfg['steering_rate'] # Server steering angle is fomula angle. But car_client depends on car. Drift type steering, normal steering, etc. Therefore, use this rate for ajust good angle.
         self.MOTOR_NEUTRAL_SPEED  = cfg['motor_neutral_speed']
-        self.MOTOR_FORWARD_SPEED_RATE = float(cfg['motor_max_speed_limit'])/float(100) # Server max speed is 100. But car_client depends on config parameter.
-        self.MOTOR_BACK_SPEED_RATE    = float(cfg['motor_min_speed_limit'])/float(100) # Server min speed is -100. But car_client depends on config parameter.
+        self.MOTOR_REVERSE = cfg['motor_reverse']
+        self.MIN_MOTOR_SPEED_LIMIT = cfg['motor_min_speed_limit']
+        self.MAX_MOTOR_SPEED_LIMIT = cfg['motor_max_speed_limit']
 
         self.steering = Servo(cfg)
         self.motor = Motor(cfg)
@@ -29,25 +31,29 @@ class RCCar():
         """
         angle: -45 to 45 degree.
         """
+        if self.STEERING_REVERSE:
+            angle = -1.0 * angle
         steering_angle = angle
         steering_angle = int(float(steering_angle) * float(self.STEERING_RATE))
         steering_angle = self.STEERING_NEUTRAL + steering_angle
         """
         Adjust within operable angle
         """
-        if steering_angle > self.MAX_STEERING_ANGLE:
-            steering_angle = self.MAX_STEERING_ANGLE
-        if steering_angle < self.MIN_STEERING_ANGLE:
-            steering_angle = self.MIN_STEERING_ANGLE
+        if steering_angle > self.MAX_STEERING_ANGLE_LIMIT:
+            steering_angle = self.MAX_STEERING_ANGLE_LIMIT
+        if steering_angle < self.MIN_STEERING_ANGLE_LIMIT:
+            steering_angle = self.MIN_STEERING_ANGLE_LIMIT
 
         self.steering.set_angle(steering_angle)
 
     def set_speed(self, speed):
+        if self.MOTOR_REVERSE:
+            speed = -1.0 * speed
         motor_speed = speed
-        if motor_speed > 0:
-            motor_speed = int(float(motor_speed) * float(self.MOTOR_FORWARD_SPEED_RATE))
-        elif motor_speed < 0:
-            motor_speed = int(float(motor_speed) * float(self.MOTOR_BACK_SPEED_RATE))
+        if motor_speed > self.MAX_MOTOR_SPEED_LIMIT:
+            motor_speed = self.MAX_MOTOR_SPEED_LIMIT
+        if motor_speed < self.MIN_MOTOR_SPEED_LIMIT:
+            motor_speed = self.MIN_MOTOR_SPEED_LIMIT
         self.motor.set_speed(motor_speed)
 
     def brake(self, value):
